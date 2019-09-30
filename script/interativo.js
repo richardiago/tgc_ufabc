2fa5356985d262cb8818c4f5e99a1a04cf4eb4e1
richardiago <iagorichard@outlook.com>
2019-09-30 10:10:54 -0300

    novas interações

--- script/interativo.js
+++ script/interativo.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 
+    var metodo = ["regressão linear","regressão linear","regressão linear","regressão logística"]
+
+    var variaveis = ["Foi utilizada a variável tom de pele para a regressão.",
+                    "As seguintes variáveis foram utilizadas para realizar a regressão: tom de pele, idade, idade², peso, peso², altura, altura².",
+                    "As seguintes variáveis foram utilizadas para realizar a regressão: tom de pele, idade, idade², peso, peso², altura, altura², liga e posição.", 
+                    "Texto para regressão logistica"]
+    
+    var resultados = []
+
+    var imagens = []
+
     var p = 0;
     var srcimg = ["./images/tabelas/1.png", "./images/tabelas/2.png", "./images/tabelas/3.png"]
     var textos = ["Somente tom de pele", "Tom de pele, idade, idade², peso, peso², altura, altura²",
@@ -10,9 +21,11 @@ $(document).ready(function () {
 
         if (e.wich == 97 || e.keyCode == 97 || e.wich == 65 || e.keyCode == 65) {
 
-            if (p == srcimg.length) p = 0
-            $('#container').text(textos[p])
-            $('#imagem').attr({ 'src': srcimg[p], 'width': tamanho[p].width, 'height': tamanho[p].height });
+            if (p == metodo.length) p = 0
+            $('#metodo').text(metodo[p])
+            $('#variaveis').text(variaveis[p])
+            // $('#container').text(textos[p])
+            // $('#imagem').attr({ 'src': srcimg[p], 'width': tamanho[p].width, 'height': tamanho[p].height });
             p++;
         }
     })
