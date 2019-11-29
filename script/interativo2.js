8964ca0bc01615b959125c0980b549a872e24a98
Iago <iagorichard@outlook.com>
2019-11-28 23:49:36 -0300

    atualizações...

--- script/interativo2.js
+++ script/interativo2.js
@@ -1,31 +1,48 @@
 $(document).ready(function () {
 
-    var v = $("input[name='var']:checked").val();
-    var p = $("input[name='pen']:checked").val();
-    var e = $("input[name='expansao']:checked").val();
+  //Valores iniciais
+  var v = $("input[name='var']:checked").val();
+  var p = $("input[name='pen']:checked").val();
+  var e = $("input[name='expansao']:checked").val();
 
-    var img = "img-" + v + "_" + p + "_" + e+".png";
-    $("#imagem").attr('src', "./tabelas2/"+img)
+  var img = "img-" + v + "_" + p + "_" + e + ".png";
+  $("#imagem").attr('src', "./tabelas2/" + img)
 
-    $(".clique").click(function () {
+  //Interação com o teclado
+  $(".clique").click(function () {
 
-        v = $("input[name='var']:checked").val();
-        p = $("input[name='pen']:checked").val();
-        e = $("input[name='expansao']:checked").val();
+    //Atualiza valores
+    v = $("input[name='var']:checked").val();
+    p = $("input[name='pen']:checked").val();
+    e = $("input[name='expansao']:checked").val();
 
-        img = "img-" + v + "_" + p + "_" + e+".png";
+    img = "img-" + v + "_" + p + "_" + e + ".png";
 
-        if($("input[name='var']:checked").val() == 4){
+    //Se opção 4 de "var" é escolhida
+    if ($("input[name='var']:checked").val() == 4) {
 
-          $("#escolhas").hide();
-          $("#imagem").attr('src', "./tabelas2/"+"imagem_unica.png")
+      $("#escolhas").hide();
+      $("#imagem").attr('src', "./tabelas2/" + "imagem_unica.png")
 
-        }
-        else{
-          $("#escolhas").show();
-          $("#imagem").attr("src", "./tabelas2/"+img)
+    }
+    else {
 
-        }
-    })
+      $("#escolhas").show();
+
+      //Se opção 1 de "e" é escolhida
+      if ($("input[name='expansao']:checked").val() == 1) {
+
+        $("#penalizacao").hide();
+        img = "img-" + v + "_" + p + "_" + ".png";
+
+      }
+
+      else {
+        $("#penalizacao").show();
+        img = "img-" + v + "_" + p + "_" + e + ".png";
+      }
+
+    }
+  })
 
 })
